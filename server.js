1b54b26527b024b0404855950b9fe8a69daae082
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2024-01-02 21:39:59 +0530

    new

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+const fetchCurrencies = async () => {
+    try {
+        const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+            params: {
+                vs_currency: 'usd',
+                order: 'market_cap_desc',
+                per_page: 100,
+                page: 1,
+                sparkline: false,
+            },
+        });
+        return response.data.map((crypto) => crypto.symbol.toUpperCase());
+    } catch (error) {
+        console.error('Error fetching currencies:', error.message);
+        if (error.response) {
+            console.error('Response status:', error.response.status);
+            console.error('Response data:', error.response.data);
+            console.error('Response headers:', error.response.headers);
+        }
+        throw error;
+    }
+};
+
+app.get('/api/currencies', async (req, res) => {
+    try {
+        const currencies = await fetchCurrencies();
+        currencies.push('USD', 'EUR'); // Add more currencies if needed
+        res.json(currencies);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
